f1d1cb6b9148961b8090b84db351c382611add07
linuxing3 <linuxing3@qq.com>
2021-11-26 07:42:59 -0500

    Commited by vagrant from /home/vagrant/.dog.emacs.d on 2021-11-26-07:42

---
+++ private.el
@@ -0,0 +1,7 @@
+
+(setq +font-family "Droid Sans Mono"
+      +font-unicode-family "WenQuanYi Micro Hei Mono"
+      +variable-pitch-family "Cantarell"
+      +fixed-pitch-family "Cantarell"
+      +font-size 16
+      +theme-list '(joker))
